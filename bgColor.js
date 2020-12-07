35628538f8a55c767c450d9a0193cc32ff01a6eb
kunwar <singh.dhadial@gmail.com>
2020-12-07 07:13:08 +0530

    grid and background optimized

---
+++ bgColor.js
@@ -0,0 +1,72 @@
+// document.querySelector(".bgColorBtn").addEventListener("click",function() {
+//     if( document.querySelector(".popUp").style.display=="block")
+//         document.querySelector(".popUp").style.display="none";
+//     else
+//         document.querySelector(".popUp").style.display="block";
+// })
+// document.querySelector(".gridBtn").addEventListener("click",function() {
+//     if( document.querySelector(".popUpGrid").style.display=="block")
+//         document.querySelector(".popUpGrid").style.display="none";
+//     else
+//         document.querySelector(".popUpGrid").style.display="block";
+// })
+
+
+
+// document.querySelector(".bg-pink").addEventListener("click",function(){
+//     document.querySelector(".content").style.backgroundColor ="#ffc0c8";
+//     myEraserColor = "#ffc0c8";
+//     console.log(ctx.strokeStyle);
+//     ctx.strokeStyle = myEraserColor;
+//     ctx.clearRect(0,0,canvas.width , canvas.height);
+//     drawPoints();
+// });
+// document.querySelector(".bg-white").addEventListener("click",function(){
+//     document.querySelector(".content").style.backgroundColor ="white";
+//     myEraserColor = "white";
+//     console.log(ctx.strokeStyle);
+//     ctx.strokeStyle = myEraserColor;
+//     ctx.clearRect(0,0,canvas.width , canvas.height);
+//     drawPoints();
+// });
+// document.querySelector(".bg-grey").addEventListener("click",function(){
+//     document.querySelector(".content").style.backgroundColor ="lightgrey";
+//     myEraserColor = "lightgrey";
+//     console.log(ctx.strokeStyle);
+//     ctx.strokeStyle = myEraserColor;
+//     ctx.clearRect(0,0,canvas.width , canvas.height);
+//     drawPoints();
+// });
+// document.querySelector(".bg-skyblue").addEventListener("click",function(){
+//     document.querySelector(".content").style.backgroundColor="skyblue";
+//     myEraserColor = "skyblue";
+//     console.log(ctx.strokeStyle);
+//     ctx.strokeStyle = myEraserColor;
+//     ctx.clearRect(0,0,canvas.width , canvas.height);
+//     drawPoints();
+// });
+
+
+
+// // let bgColorDiv = document.querySelectorAll(".bg");
+// // for(let i=0; i<4; i++){
+// //     bgColorDiv[i].addEventListener("click",function(){
+// //         if(bgColorDiv[i].classList.contains("bg-pink")){
+// //             ctx.strokeStyle = "pink";
+// //         }
+// //         else if(bgColorDiv[i].classList.contains("bg-white")){
+// //             ctx.strokeStyle = "white";
+// //         }
+// //         else if(bgColorDiv[i].classList.contains("bg-grey")){
+// //             ctx.strokeStyle = "lightgrey";
+// //         }
+// //         else{
+// //             ctx.strokeStyle = "skyblue";
+// //         }
+// //         myEraserColor = ctx.strokeStyle; 
+
+// //         ctx.clearRect(0,0,canvas.width , canvas.height);
+// //         drawPoints();
+
+// //     })
+// // }
